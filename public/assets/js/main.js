c1dd709a8e78dd2abe519736ae9891df99d212cd
iftekher2108 <parvezkhan21082002@gmail.com>
2024-08-08 16:15:57 +0600

    mejor change and sneat theme added

---
+++ public/assets/js/main.js
@@ -0,0 +1,41 @@
+/**
+ * Main
+ */
+
+
+
+
+$(document).ready(function() {
+
+    
+$('.menu-toggle').click(function(){
+    $(this).siblings('.menu-sub').slideToggle(300)
+})
+
+
+
+
+
+
+
+
+})
+
+
+  // Accordion active class
+  const accordionActiveFunction = function (e) {
+    if (e.type == 'show.bs.collapse' || e.type == 'show.bs.collapse') {
+      e.target.closest('.accordion-item').classList.add('active');
+    } else {
+      e.target.closest('.accordion-item').classList.remove('active');
+    }
+  };
+
+  const accordionTriggerList = [].slice.call(document.querySelectorAll('.accordion'));
+  const accordionList = accordionTriggerList.map(function (accordionTriggerEl) {
+    accordionTriggerEl.addEventListener('show.bs.collapse', accordionActiveFunction);
+    accordionTriggerEl.addEventListener('hide.bs.collapse', accordionActiveFunction);
+
+
+  });
+
